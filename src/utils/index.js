b489d054e421bccb7ea0f3ed8bbc303e307513ef
MengWangwang <1275534396@qq.com>
2019-11-30 16:24:18 +0800

    实现了后端数据流文件的接收与下载，添加了文件名称生成方法并引用

--- src/utils/index.js
+++ src/utils/index.js
@@ -56,3 +56,20 @@ export function clearLoginInfo () {
   store.commit('resetStore')
   router.options.isAddDynamicMenuRoutes = false
 }
+
+/** 随机生成固定位数或者一定范围内的字符串数字组合
+  * @param {Number} min 范围最小值
+  * @param {Number} max 范围最大值，当不传递时表示生成指定位数的组合
+  * @returns {String} 返回字符串结果
+  * */
+export function randomRange(min, max) {
+  var returnStr = "",
+  range = (max ? Math.round(Math.random() * (max-min)) + min : min),
+  arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  for(var i=0; i<range; i++){
+    var index = Math.round(Math.random() * (arr.length-1));
+    returnStr += arr[index];
+  }
+  return returnStr;
+}
--- src/views/modules/express/qrcode.vue
+++ src/views/modules/express/qrcode.vue
@@ -1,36 +1,42 @@
 <template>
   <div>
       <el-button v-if="isAuth('express:qrcode:print')" type="primary" @click="pintQRCode()">打印二维码</el-button>
-      <a class='download' :href='downloadhttp' download="" title="下载">下载</a>
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
+  import { randomRange } from '@/utils'
   export default {
     data () {
       return {
         downloadhttp : ""
       }
     },
-    
     methods: {
       // 新增 / 修改
       pintQRCode () {
        this.$http({
           url: this.$http.adornUrl('/express/qrcode/print'),
-          method: 'get',
+          method: 'post',
           params: this.$http.adornParams({
             'key': "123"
-          })
-        }).then(({data}) => {
-          if (data && data.code === 0) {
-              console.log(data.filepath);
-              this.downloadhttp=data.filepath;
-          } else {
-            console.log(data);
-          }
-          
+          }),
+          // 二进制流文件，一定要设置成blob，默认是json
+          responseType: 'blob'
+
+        }).then(res => {
+          const link = document.createElement('a')
+          const blob = new Blob([res.data], { type: 'application/pdf' })
+          link.style.display = 'none'
+          link.href = URL.createObjectURL(blob)
+
+          let fileName = randomRange(10);
+
+          console.log(fileName);
+          link.setAttribute('download', fileName+".pdf")
+          document.body.appendChild(link)
+          link.click()
+          document.body.removeChild(link)
         })
       },
     }
